2a231ae83b65faa53e52a431bbf8ce0efe050e73
Umar Niazi <uhkniazi@gmail.com>
2016-08-26 11:44:40 +0100

    changed to 2 group problem instead of 3

--- GSE44270/de_analysis_simulated.R
+++ GSE44270/de_analysis_simulated.R
@@ -1,4 +1,4 @@
-# Name: de_analysis.R
+# Name: de_analysis_simulated.R
 # Auth: Umar Niazi 
 # Date: 12/08/2016
 # Desc: analysis of the downloaded data
@@ -21,77 +21,6 @@ p.old = par()
 
 ########################
 
-######################### functions
-# Name: f_Plot3DPCA
-# Args: mComp = n X 3 matrix with first 3 components as the 3 column vectors
-#       color = colours for the points
-#       ... additional arguments to the plot function
-# Rets: none
-# Desc: takes the first 3 components and plots the data in a 3d plot
-f_Plot3DPCA = function(mComp, color, ...) {
-  x = mComp[,1]
-  y = mComp[,2]
-  z = mComp[,3]
-  if (!require(scatterplot3d)) stop('scatterplot3d library required')
-  scatterplot3d(x, y, z, color, ...)
-}
-
-
-# Function: f_lGetPCAClusterCount
-# Desc: Takes first two components of the PCA and counts possible clusters. The function does this by 
-#       binning the vector of data into X bins, assigning a class label to each bin, counting how many
-#       observations in each bin, any total number of bins with at least one observations. This is calculated
-#       for both the components of the pca matrix, and the max number of bins with at least one observation, in
-#       first or second dimension is reported along with a data.frame with cluster labels.
-# Args: pr.out = principal component object returned by prcomp function
-# Rets: returns list with 2 elements: 
-#       1 - cluster.count = possible number of clusters in the data
-#       2 - cluster.label = data.frame with cluster labels
-f_lGetPCAClusterCount = function(pr.out){
-  # how many clusters in data, using first 2 components
-  x1 = pr.out$x[,1]
-  x2 = pr.out$x[,2]
-  # bin the data from the 2 components
-  h1 = hist(x1, plot=F)
-  # give a class label to each bin
-  c1 = cut(x1, h1$breaks, labels = 1:(length(h1$mids)))
-  h2 = hist(x2, plot=F)
-  c2 = cut(x2, h2$breaks, labels = 1:(length(h2$mids)))
-  # labels for vectors and the class labels
-  dfClust = data.frame(lab=names(x1), c1, c2)
-  # get contingency table
-  mClust = as.matrix(table(c1 = dfClust$c1, c2 = dfClust$c2))
-  # count the max of row and col sums that are not zero
-  ir = length(which(rowSums(mClust) != 0))
-  ic = length(which(colSums(mClust) != 0))
-  iClust.count = ifelse(ir > ic, ir, ic)
-  lRet = list(cluster.count=iClust.count, cluster.label=dfClust)
-  return(lRet)
-}
-
-
-f_plotVolcano = function(dfGenes, main, p.adj.cut = 0.1, fc.lim = c(-3, 3)){
-  p.val = -1 * log10(dfGenes$P.Value)
-  fc = dfGenes$logFC
-  # cutoff for p.value y.axis
-  y.cut = -1 * log10(0.01)
-  col = rep('lightgrey', times=length(p.val))
-  c = which(dfGenes$adj.P.Val < p.adj.cut)
-  col[c] = 'red'
-  plot(fc, p.val, pch=20, xlab='Fold Change', ylab='-log10 P.Value', col=col, main=main, xlim=fc.lim)
-  abline(v = 0, col='grey', lty=2)
-  abline(h = y.cut, col='red', lty=2)
-  # second cutoff for adjusted p-values
-  y.cut = quantile(p.val[c], probs=0.95)
-  abline(h = y.cut, col='red')
-  # identify these genes
-  g = which(p.val > y.cut)
-  lab = dfGenes[g, 'SYMBOL']
-  text(dfGenes$logFC[g], y = p.val[g], labels = lab, pos=2, cex=0.6)
-}
-
-
-######################### end functions
 
 ## data loading step
 # connect to mysql database to find file locations
@@ -130,7 +59,7 @@ dfAnnotation$cvGenes = cvGenes
 dim(x.affy)
 # create a subset of data based on cell type
 i = which(x.affy$fCell.type == 'fibroblast')
-x.affy = x.affy[,-i]
+x.affy = x.affy[,i]
 dim(x.affy)
 mDat = exprs(x.affy)
 # replace probe names by enterez ids
@@ -167,6 +96,11 @@ identical(rownames(dfSamples), colnames(mDat))
 
 ## create grouping factors
 fSamples = dfSamples$fDisorder
+fSamples = as.character(fSamples)
+i = which(fSamples == 'healthy')
+fSamples[i]
+fSamples[i] = 'keloid.non-lesion'
+fSamples = factor(fSamples)
 table(fSamples)
 levels(fSamples)
 
@@ -190,22 +124,44 @@ get.pairwise.prob = function(x, fac, p.adjust.method='none'){
   return(out)
 }
 
+## get p.value from simulated data
+get.prob = function(x, f){
+  # get the baseline
+  cBaseline = levels(f)[1]
+  ivBaseline = x[which(f == cBaseline)]
+  # remaining levels/groups to compare against
+  cvLevels = levels(f)[-1]
+  pret = sapply(cvLevels, function(l){
+    x.l = x[which(f == l)]
+    x.l.m = mean(x.l)
+    # calculate two sided p-value
+    return(min(c(sum(ivBaseline <= x.l.m)/length(ivBaseline), sum(ivBaseline >= x.l.m)/length(ivBaseline))) * 2)
+  })
+  return(pret)
+}
+
+
 # simulate a posterior mean for each gene
-lComparison = apply(mDat, 1, f_lpostMean, fSamples, sim.size = 1000)
+lComparison = apply(mDat, 1, f_lpostMean, fSamples, sim.size = 10000)
 # for each member of list perform comparison
 lPvalue = lapply(lComparison, function(x){
   df = data.frame(do.call(cbind, x))
   st = stack(df)
-  return(get.pairwise.prob(st$values, st$ind, p.adjust.method = 'BH'))
+  return(get.prob(st$values, st$ind))
 })
 
-ind = sapply(lPvalue, function(x){
-  any(na.omit(x <= 0.01))
-})
+ivPvalue = unlist(lPvalue)
+names(ivPvalue) = names(lPvalue)
+ivPvalue.adj = p.adjust(ivPvalue, 'BH')
+table(ivPvalue.adj < 0.1)
+table(ivPvalue.adj < 0.01)
 
-lPvalue.sig = lPvalue[ind]
+# ind = sapply(lPvalue, function(x){
+#   any(na.omit(x <= 0.01))
+# })
+# 
+# lPvalue.sig = lPvalue[ind]
 
-####### end grouping
 # 
 # ################################### main analysis section
 # 
@@ -268,7 +224,7 @@ lPvalue.sig = lPvalue[ind]
 # mCommonGenes = sapply(seq_along(n), function(x) cvCommonGenes %in% lSigGenes.adj[[names(n[x])]])
 # rownames(mCommonGenes) = cvCommonGenes
 # colnames(mCommonGenes) = names(n)
-cvCommonGenes = names(lPvalue.sig)
+cvCommonGenes = names(ivPvalue.adj[ivPvalue.adj < 0.01])
 
 #### analysis by grouping genes
 # create groups in the data based on 2^n-1 combinations
@@ -475,7 +431,7 @@ if (length(i) > 0) {
 set.seed(1)
 ig = plot.centrality.graph(oGr)
 # plot the genes or vertex sizes by fold change
-ig = f_igCalculateVertexSizesAndColors(ig, t(mCounts), fGroups, bColor = F, iSize = 10)
+ig = f_igCalculateVertexSizesAndColors(ig, t(mCounts), fGroups, bColor = F, iSize = 20)
 set.seed(1)
 plot(ig, vertex.label=NA, layout=layout_with_fr, vertex.frame.color=NA, edge.color='darkgrey')
 par(p.old)
@@ -672,15 +628,15 @@ dfCluster = cbind(dfCluster[as.character(df$ENTREZID),], SYMBOL=df$SYMBOL, GENEN
 write.csv(dfCluster, file='Temp/ClustersToGeneMapping.xls')
 
 # save the graph and data objects
-keloid_keratinocyte_data = list(graph=oGr, matrix=mCounts, groups=fGroups)
-save(keloid_keratinocyte_data, file='Objects/keloid_keratinocyte_data.rds')
+keloid_fibroblast_data = list(graph=oGr, matrix=mCounts, groups=fGroups)
+save(keloid_fibroblast_data, file='Objects/keloid_fibroblast_data_2_gps.rds')
 
 library('RMySQL')
 db = dbConnect(MySQL(), user='rstudio', password='12345', dbname='Projects', host='127.0.0.1')
 dbListTables(db)
 dbListFields(db, 'MetaFile')
-df = data.frame(idData=4, name='keloid_keratinocyte_data.rds', type='rds', 
-                location='~/Data/MetaData/', comment='GSE44270 graph object, expression matrix and grouping factor for keratinocytes')
+df = data.frame(idData=4, name='keloid_fibroblast_data_2_gps.rds', type='rds', 
+                location='~/Data/MetaData/', comment='GSE44270 graph object, expression matrix and grouping factor for fibroblasts, but this analysis is with only 2 groups after merging keloid.non-lesion and normal.')
 dbWriteTable(db, name = 'MetaFile', value=df, append=T, row.names=F)
 dbDisconnect(db)
 
