32440c96339dec03a8acdc2ff252abe50476565e
Umar Niazi <uhkniazi@gmail.com>
2016-08-31 16:49:47 +0100

    shell script changed

--- Keloid_main/trimmomatic.sh
+++ Keloid_main/trimmomatic.sh
@@ -4,7 +4,13 @@
 # Desc: shell script to run trimmomatic on a list of files
 
 #!/bin/bash
-
+#$ -S /bin/sh
+#$ -pe smp 1 
+#$ -cwd
+#$ -N trim-test
+#$ -j y
+#$ -l mem_free=8G
+#$ -l h_rt=02:00:00
 # check if empty commandline
 if [ "$#" -ne 2 ]
 then
@@ -12,7 +18,11 @@ then
     exit 0
 fi
 
+module load general/JRE/1.8.0_65
+module load bioinformatics/trimmomatic/0.36
+
 #INPUT_samples="$@"
 
-echo "java -jar trimmomatic-0.35.jar PE -phred33 $1 $2 $1_trim.fq.gz $1_unpaired.fq.gz $2_trim.fq.gz $2_unpaired.fq.gz ILLUMINACLIP:TruSeq3-PE.fa:2:30:10:8:true LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36"
+java -jar /opt/apps/bioinformatics/trimmomatic/0.36/trimmomatic-0.36.jar PE -phred33 $1 $2 $1_trim.fq.gz $1_unpaired.fq.gz $2_trim.fq.gz $2_unpaired.fq.gz ILLUMINACLIP:/users/k1625253/brc_scratch/Data/MetaData/trimmomatic_adapters.fa:2:30:10:8:true LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+
 
