7022bcdb637a9854b634fc99ca8b24b03721f6c1
Umar Niazi <uhkniazi@gmail.com>
2016-11-24 10:25:29 +0000

    s021 bam qa done

--- Keloid_main/bam_files_qa.R
+++ Keloid_main/bam_files_qa.R
@@ -29,7 +29,7 @@ dbListFields(db, 'File')
 # get the query
 q = paste0('select Sample.id as sid, Sample.group1 as timepoints, Sample.title, File.* from Sample, File
 where (Sample.idData = 2) AND (Sample.group1 like "%Timepoint%") AND (File.idSample = Sample.id AND File.type like "%duplicates removed%" 
-AND File.group1 = "S014")')
+AND File.group1 = "S021")')
 dfSample = dbGetQuery(db, q)
 # close connection after getting data
 dbDisconnect(db)
@@ -38,7 +38,7 @@ dfSample$name = gsub(" ", "", dfSample$name, fixed = T)
 dfSample$title = gsub(" ", "", dfSample$title, fixed = T)
 
 #### set working directory to appropriate location with bam files
-setwd('Data_external/Aligned/S014/')
+setwd('Data_external/Aligned/S021/')
 csFiles = list.files('.', pattern = '*.bam')
 # check if these files match the file names in database
 table(dfSample$name %in% csFiles)
@@ -62,9 +62,9 @@ lAllBams = lapply(csFiles, function(x){
 })
 names(lAllBams) = dfSample$title
 setwd(gcswd)
-n = make.names(paste('CBamScaffold rd S014 rds'))
+n = make.names(paste('CBamScaffold rd S021 spikein rds'))
 lAllBams$meta = dfSample
-lAllBams$desc = paste('CBamScaffold object from S014 sequencing run with quality 10 duplicates removed', date())
+lAllBams$desc = paste('CBamScaffold object from S021 sequencing run with spikein and quality 10 duplicates removed', date())
 n2 = paste0('~/Data/MetaData/', n)
 save(lAllBams, file=n2)
 
@@ -73,7 +73,7 @@ db = dbConnect(MySQL(), user='rstudio', password='12345', dbname='Projects', hos
 dbListTables(db)
 dbListFields(db, 'MetaFile')
 df = data.frame(idData=2, name=n, type='rds', location='~/Data/MetaData/', 
-                comment='CBamScaffold object from S014 sequencing run with quality 10 duplicates removed')
+                comment='CBamScaffold object from S021 sequencing run with spikein quality 10 duplicates removed')
 dbWriteTable(db, name = 'MetaFile', value=df, append=T, row.names=F)
 dbDisconnect(db)
 
@@ -81,7 +81,7 @@ dbDisconnect(db)
 getwd()
 lAllBams$desc = NULL
 lAllBams$meta = NULL
-pdf(file='Keloid_main/Results/bam.q10.rd.qa.S014.pdf')
+pdf(file='Keloid_main/Results/bam.q10.rd.qa.S021.pdf')
 par(mfrow=c(2,2))
 ## returns the coverage for each scaffold in the bam file in matrix
 f1 = function(ob){
@@ -125,7 +125,7 @@ f1 = function(ob){
 
 ## call the function f1
 ivMat = sapply(lAllBams, f1)
-barplot(ivMat, las=2, main='Sequencing run S014 - Average Read Width', ylab = 'Average Read Width', cex.names =0.6)
+barplot(ivMat, las=2, main='Sequencing run S021 - Average Read Width', ylab = 'Average Read Width', cex.names =0.6)
 
 # number of reads aligned
 f1 = function(ob){
@@ -136,7 +136,7 @@ f1 = function(ob){
 
 iReadCount = sapply(lAllBams, f1)
 
-barplot(iReadCount, las=2, main='Sequencing run S014 - Read Count', ylab = 'No. of Reads in Millions', cex.names =0.6)
+barplot(iReadCount, las=2, main='Sequencing run S021 - Read Count', ylab = 'No. of Reads in Millions', cex.names =0.6)
 
 # average binned coverage distribution
 f1 = function(ob){
@@ -149,14 +149,40 @@ f1 = function(ob){
 ## call the function f1
 lMat = lapply(lAllBams, f1)
 
-boxplot(lMat, las=2, main='Sequencing run S014 - Average Binned Coverage', ylab = 'Average', outline=F, xaxt='n')
+boxplot(lMat, las=2, main='Sequencing run S021 - Average Binned Coverage', ylab = 'Average', outline=F, xaxt='n')
 axis(1, at=1:length(lMat), labels = names(lMat), cex.axis=0.7, las=2)
+
+
+
+## take the average of the coverage for grouping factors
+## creating a cleaner single figure for coverage
+mCov = lMat.ordered[['chr2']]
+dim(mCov)
+colnames(mCov)
+# match title to colnames
+i = match(dfSample$title, colnames(mCov))
+dfSample = dfSample[i,]
+
+fac1 = gsub('^(K|N).+', '\\1', dfSample$title)
+fac2 = gsub('.+(1|2).+', '\\1', dfSample$title)
+fac = factor(paste(fac1, fac2, sep='-'))
+
+m = sapply(levels(fac), function(x){
+  i = which(fac == x)
+  mCov.sub = rowMeans(mCov[,i])
+  return(mCov.sub)
+})
+
+m = apply(m, 2, function(y) lowess(y, f=2/10)$y )
+iCol = rainbow(nlevels(fac))
+matplot(m, type='l', main=paste('Lowess fit to coverage', 'chr2'), col=iCol, xlab='Bins', sub='Binned Coverage', ylab='Coverage', 
+        lty=1, lwd=2)
+
+legend('bottomright', legend = colnames(m), ncol=1, col = iCol, lty=1, cex=1, lwd=2)
 
 dev.off(dev.cur())
 
-
-
-# bf = BamFile('/run/user/1000/gvfs/sftp:host=10.202.64.28,user=k1625253/users/k1625253/Data/ProjectsData/BRC_Keloid/Aligned/S014/K2-2nd_S014_R1_.fastq.gz_q10_sort_rd.bam')
+# bf = BamFile('/run/user/1000/gvfs/sftp:host=10.202.64.28,user=k1625253/users/k1625253/Data/ProjectsData/BRC_Keloid/Aligned/S021/K2-2nd_S021_R1_.fastq.gz_q10_sort_rd.bam')
 # si = seqinfo(bf)
 # si = as.data.frame(si)
 # si = si[order(si$seqlengths, decreasing = T),]
